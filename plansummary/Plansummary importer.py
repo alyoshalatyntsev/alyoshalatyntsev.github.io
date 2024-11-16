36f73000873fc5420daddaa06d02ab14ca13c81f
alyoshalatyntsev <78869167+alyoshalatyntsev@users.noreply.github.com>
2024-11-16 17:51:46 +0100

    summary

---
+++ plansummary/Plansummary importer.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+
+# Define source and updated destination paths
+source_path = r"C:\Users\alyos\Dropbox\NOTES\Research evil scheming\RESSummary.pdf"
+destination_path = r"C:\Users\alyos\Dropbox\alyoshalatyntsev.github.io\plansummary\plansummary.pdf"
+
+# Check if the source file exists
+if os.path.exists(source_path):
+    try:
+        shutil.copyfile(source_path, destination_path)
+        print("File copied successfully.")
+    except Exception as e:
+        print(f"Error during file copy: {e}")
+else:
+    print("Source file does not exist.")
